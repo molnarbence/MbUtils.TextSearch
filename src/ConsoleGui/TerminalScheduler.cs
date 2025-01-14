9bdeb9773ef7ecbeca96a7702a4592dc8109496a
Bence Molnár <xjarekx@gmail.com>
2025-01-14 21:02:05 +0100

    Cleanup

--- src/ConsoleGui/TerminalScheduler.cs
+++ src/ConsoleGui/TerminalScheduler.cs
@@ -6,54 +6,54 @@ namespace ConsoleGui;
 
 public class TerminalScheduler : LocalScheduler
 {
-    public static readonly TerminalScheduler Default = new ();
-    private TerminalScheduler () { }
+    public static readonly TerminalScheduler Default = new();
 
-    public override IDisposable Schedule<TState> (
+    private TerminalScheduler()
+    {
+    }
+
+    public override IDisposable Schedule<TState>(
         TState state,
         TimeSpan dueTime,
         Func<IScheduler, TState, IDisposable> action
     )
     {
-        IDisposable PostOnMainLoop ()
-        {
-            var composite = new CompositeDisposable (2);
-            var cancellation = new CancellationDisposable ();
-
-            Application.Invoke (
-                                () =>
-                                {
-                                    if (!cancellation.Token.IsCancellationRequested)
-                                    {
-                                        composite.Add (action (this, state));
-                                    }
-                                }
-                               );
-            composite.Add (cancellation);
-
-            return composite;
-        }
-
-        IDisposable PostOnMainLoopAsTimeout ()
-        {
-            var composite = new CompositeDisposable (2);
-
-            object timeout = Application.AddTimeout (
-                                                     dueTime,
-                                                     () =>
-                                                     {
-                                                         composite.Add (action (this, state));
-
-                                                         return false;
-                                                     }
-                                                    );
-            composite.Add (Disposable.Create (() => Application.RemoveTimeout (timeout)));
-
-            return composite;
-        }
-
         return dueTime == TimeSpan.Zero
-                   ? PostOnMainLoop ()
-                   : PostOnMainLoopAsTimeout ();
+            ? PostOnMainLoop()
+            : PostOnMainLoopAsTimeout();
+
+        IDisposable PostOnMainLoop()
+        {
+            var composite = new CompositeDisposable(2);
+            var cancellation = new CancellationDisposable();
+
+            Application.Invoke(
+                () =>
+                {
+                    if (!cancellation.Token.IsCancellationRequested) composite.Add(action(this, state));
+                }
+            );
+            composite.Add(cancellation);
+
+            return composite;
+        }
+
+        IDisposable PostOnMainLoopAsTimeout()
+        {
+            var composite = new CompositeDisposable(2);
+
+            var timeout = Application.AddTimeout(
+                dueTime,
+                () =>
+                {
+                    composite.Add(action(this, state));
+
+                    return false;
+                }
+            );
+            composite.Add(Disposable.Create(() => Application.RemoveTimeout(timeout)));
+
+            return composite;
+        }
     }
 }
